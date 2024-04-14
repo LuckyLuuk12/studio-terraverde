e4446f3af3a8e4cba5b91ec0ca22f9430c7eb0a0
LuckyLuuk12 <luckyluuk02@gmail.com>
2024-04-14 19:45:01 +0200

    feat: changed image location to static folder

--- svelte/src/lib/data.ts
+++ svelte/src/lib/data.ts
@@ -1,40 +1,47 @@
 import { writable } from 'svelte/store';
-import type { ProjectCategory } from '$lib/types';
+import type { ProjectCategory, Project } from '$lib/types';
 
 export const categories = writable<ProjectCategory[]>([
 	{
 		title: 'Schools',
 		content: 'You should put some text about school related projects here.',
-		image: '/src/routes/images/categories/cat1.png'
+		image: '/images/categories/cat1.png'
 	},
 	{
 		title: 'Offices',
 		content: 'You should put some text about office related projects here.',
-		image: '/src/routes/images/categories/cat2.png'
+		image: '/images/categories/cat2.png'
 	},
 	{
 		title: 'Cultural Projects',
 		content: 'You should put some text about cultural related projects here. Like churches',
-		image: '/src/routes/images/categories/cat3.png'
+		image: '/images/categories/cat3.png'
 	},
 	{
 		title: 'Student Guidance',
 		content: 'You should put some text here about student guidance things you did',
-		image: '/src/routes/images/categories/cat1.png'
+		image: '/images/categories/cat1.png'
 	},
 	{
 		title: 'Local Authority Projects',
 		content: 'You should put some text here about the projects you did for the local authorities',
-		image: '/src/routes/images/categories/cat3.png'
+		image: '/images/categories/cat3.png'
 	},
 	{
 		title: 'Other projects',
 		content: 'You should put all other projects here',
-		image: '/src/routes/images/categories/cat4.png'
+		image: '/images/categories/cat4.png'
 	},
 	{
 		title: 'Unrealized projects',
 		content: 'You should put text about unrealized projects here',
-		image: '/src/routes/images/categories/cat2.png'
+		image: '/images/categories/cat2.png'
 	}
 ]);
+
+// export const projects = writable<Project[]>([
+// 	{
+// 		title: 'Project 1',
+// 		description: 'This is the first project
+// 	}
+// ]);
